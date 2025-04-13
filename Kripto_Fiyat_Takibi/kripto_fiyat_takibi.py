cb8a7329c52fb93a0ec31f17e7a549034aa45df8
SertEmre <emresert2233@gmail.com>
2025-04-14 00:17:47 +0300

    CoinGecko API ile en popüler 100 coin’i piyasa değerine göre sıralayıp isim, sembol ve güncel fiyatlarını listeleyecek şekilde kod güncellendi.

--- Kripto_Fiyat_Takibi/kripto_fiyat_takibi.py
+++ Kripto_Fiyat_Takibi/kripto_fiyat_takibi.py
@@ -1,4 +1,27 @@
 import requests
+
+def popüler_coinler_listesi():
+    url = 'https://api.coingecko.com/api/v3/coins/markets' 
+    params = {
+        'vs_currency': 'usd',
+        'order': 'market_cap_desc',
+        'per_page': 100,
+        'page':1
+    }
+
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        tüm_coinler = response.json()
+        popüler_coinler = [coin['id']for coin in tüm_coinler[:100]]
+        print("\nEn popüler 100 coin:")
+        for i, coin in enumerate(popüler_coinler, 1):
+            print(f"{i}.{coin}")
+        return popüler_coinler
+    else:
+        print("Popüler coin listesi alınamadı.")
+        return
+
 def kripto_fiyat_takibi(coin,para):
     url='https://api.coingecko.com/api/v3/simple/price'
     params = {
@@ -17,8 +40,25 @@ def kripto_fiyat_takibi(coin,para):
     else:
         print("Fiyat alınamadı.")
 
-coin = input("Coinin ismini giriniz: ").lower()
-para = input("Para birimini giriniz: ").lower()
+def ana_program():
+    popüler_coinler = []
+    while True:
+        print("1-Popüler coinleri listele")
+        print("2-Coin fiyatını sorgula")
+        print("3-Çıkış yap")
 
-kripto_fiyat_takibi(coin, para)
+        seçim = input("Yapacağınız işlemi seçiniz:").strip()
 
+        if seçim == '1':
+            popüler_coinler = popüler_coinler_listesi()
+        elif seçim == '2':
+            coin = input("Coinin ismini giriniz: ").lower()
+            para = input("Para birimini giriniz: ").lower()
+            kripto_fiyat_takibi(coin, para)
+        elif seçim == '3':
+            print("Programdan çıkılıyor. Görüşmek üzere!")
+            break
+        else:
+            print("Geçersiz seçim. Lütfen tekrar deneyiniz.")
+
+ana_program()
---
+++ README.md
@@ -0,0 +1,4 @@
+CoinGecko API kullanarak
+En popüler ilk 100 kripto parayı piyasa değerine göre listeler ve her bir coin'in ismini, sembolünü ve güncel fiyatını gösterir
+
+Program internetten canlı veri çektiği için bazı ağlarda (örneğin "Güvenli İnternet" profili açıkken) çalışmayabilir. Lütfen uygulamayı kullanırken internet bağlantınızın dış kaynaklara erişim sağladığından emin olun.
